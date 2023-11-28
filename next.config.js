f6dd1a5f6f35e60ff6426c7490302b0db77c3070
Carlos Faustino <chfcchfc96@gmail.com>
2023-11-28 16:20:09 -0300

    add images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "raw.githubusercontent.com",
+        protocol: "https"
+      }
+    ]
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
