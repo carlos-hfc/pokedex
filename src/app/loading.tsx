d23064386680374dcc70623e81fec8bf763d330e
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-04 17:06:52 -0300

    add loader and animations

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="loader flex items-center justify-center inset-0 fixed w-full">
+      <div className="loader-container w-1/3 h-1/3" />
+    </div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from "react";
 
+import Loading from "./loading";
+
 import { SearchPokemon } from "@/@types";
 
 import { Filter } from "@/components/Filter";
@@ -8,7 +10,7 @@ import { Pokemon } from "@/components/Pokemon";
 
 import { FilterProvider } from "@/contexts/FilterContext";
 
-import { getAll } from "@/services/fetches";
+import { getAll, getTypes } from "@/services/fetches";
 
 import '@/styles/main.min.css';
 
@@ -19,15 +21,16 @@ export default async function Home({
   searchParams: SearchPokemon;
 }) {
   const pokemons = await getAll(searchParams);
+  const types = await getTypes();
 
   return (
     <FilterProvider>
-      <main className="flex min-h-screen flex-col mx-auto container max-w-7xl px-4 lg:px-8 gap-8">
-        <div className="flex flex-row h-auto items-center justify-between">
-          <Filter />
-        </div>
+      <Suspense key={searchParams.page + searchParams.name + types + pokemons.data} fallback={<Loading />}>
+        <main className="flex min-h-screen flex-col mx-auto container max-w-7xl px-4 lg:px-8 gap-8 py-8">
+          <div className="flex flex-row h-auto items-center justify-between">
+            <Filter types={types} />
+          </div>
 
-        <Suspense key={searchParams.page + searchParams.name} fallback="loading">
           <div className="grid grid-flow-row-dense md:grid-cols-2 xl:grid-cols-3 gap-8 md:gap-x-6 md:gap-y-8">
             {pokemons.data?.map((p, i) => (
               <Pokemon
@@ -36,10 +39,12 @@ export default async function Home({
               />
             ))}
           </div>
-        </Suspense>
 
-        <Pagination totalCount={pokemons.total} />
-      </main>
+          <div className="flex items-center justify-center py-8">
+            <Pagination totalCount={pokemons.total} />
+          </div>
+        </main>
+      </Suspense>
     </FilterProvider>
   );
 }
