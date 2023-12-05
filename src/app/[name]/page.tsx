18f569e838a6af9bb58370b539786d6c2130c9cf
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-05 17:26:22 -0300

    starting layout

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata, ResolvingMetadata } from "next";
+import Image from "next/image";
 
+import { PokemonType } from "@/@types";
 import { getByName } from "@/services/fetches";
 
 interface Props {
@@ -13,16 +15,59 @@ interface Props {
 
 export async function generateMetadata({ params }: Props, parent: ResolvingMetadata): Promise<Metadata> {
   return {
-    title: params.name.charAt(0).toUpperCase() + params.name.slice(1),
+    title: params.name.charAt(0).toUpperCase() + params.name.slice(1)
   };
 }
 
 export default async function Pokemon({ params }: { params: { name: string; }; }) {
-  const pokemon = await getByName(params.name);
+  const pokemon = await getByName<PokemonType>(params.name);
+
+  const firstType = pokemon.types[0].type.name;
 
   return (
-    <pre>
-      {pokemon.name}
-    </pre>
+    <main className="pt-40 px-4 md:pt-36 lg:px-8">
+      <div className="container mx-auto">
+        <header className="flex flex-col items-center justify-center">
+          <h1 className="text-white capitalize font-bold text-4xl flex flex-wrap items-center justify-center gap-4">
+            {pokemon.name}
+
+            <span className="text-slate-300 font-semibold">
+              #{String(pokemon.id).padStart(4, '0')}
+            </span>
+          </h1>
+
+        </header>
+        {/* <div className="relative h-64 w-full">
+            <Image
+              src={pokemon?.sprites?.other?.dream_world?.front_default ?? pokemon?.sprites?.other?.home?.front_default ?? pokemon?.sprites?.other?.["official-artwork"]?.front_default ?? ""}
+              alt={pokemon.name}
+              fill
+              loading="lazy"
+              className="object-contain"
+            />
+          </div>
+
+          <div className="w-full flex flex-col items-center justify-center">
+            <div className="font-bold text-white">
+              #{String(pokemon.id).padStart(4, '0')}
+            </div>
+
+            <h1 className="capitalize font-bold text-white text-4xl drop-shadow-text">
+              {pokemon.name}
+            </h1>
+
+            <div className="flex flex-row">
+              {pokemon.types.map((item, i) => (
+                <span
+                  key={`${item}-${i}`}
+                  className={`bg-${item.type.name} px-3 py-0.5 rounded text-white shadow-lg`}
+                >
+                  {item.type.name}
+                </span>
+              ))}
+            </div>
+          </div> */}
+      </div>
+    </main>
   );
 }
