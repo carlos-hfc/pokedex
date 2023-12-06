3e9c1cd5df5b97b7fe1872b116842db29724d672
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-06 17:24:09 -0300

    exception to get generation one randomly

--- src/app/api/feature/route.ts
+++ src/app/api/feature/route.ts
@@ -1,14 +1,29 @@
 import { NextResponse } from "next/server";
 
+import { Generation } from "@/@types";
 import { api } from "@/services/api";
 import { getAll } from "@/services/fetches";
+
+function randomize(total: number) {
+  return (Math.random() * (total - 1) + 1).toFixed(0);
+}
 
 export async function GET() {
   const { total } = await getAll({ page: '0' });
 
-  const random = (Math.random() * (total - 1) + 1).toFixed(0);
+  const random = randomize(total);
 
-  const feature = await api.get(`/pokemon/${random}`);
+  try {
+    const response = await api.get(`/pokemon/${random}`);
 
-  return NextResponse.json(feature.data)
+    return NextResponse.json(response.data);
+  } catch (error) {
+    const generation = await api.get<Generation>("/generation/1");
+
+    const random = randomize(generation.data.pokemon_species.length);
+
+    const pokemon = await api.get(`/pokemon/${random}`);
+
+    return NextResponse.json(pokemon.data);
+  }
 }
