e4c1ba561aeb96f755f865d2693623b146ac24a8
Carlos Faustino <chfcchfc96@gmail.com>
2023-12-06 17:24:42 -0300

    absolute only in home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,25 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Header() {
+  const pathname = usePathname();
+
   return (
-    <header className="absolute top-0 left-0 w-full z-10">
+    <header className={`w-full ${pathname === '/' ? "absolute top-0 left-0 z-10" : ""}`}>
       <nav
-        className="container max-w-7xl mx-auto p-4 lg:p-8 items-center justify-center flex"
+        className="container max-w-7xl mx-auto p-4 lg:px-8 items-center justify-center flex"
         aria-label="Header"
       >
-        <Link href="/">
+        <Link href="/" scroll={false}>
           <Image
             src="/pokemon.png"
             alt="Pokémon logo"
             width={370}
             height={136}
-            className="max-w-full xs:w-64 saturate-0"
+            className="max-w-full w-72 saturate-0"
           />
         </Link>
       </nav>
